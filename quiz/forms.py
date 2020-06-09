fe4ce23da4cd8aa1ea712da190caa00e4aba6740
SaifullahUsmani693 <amjadmehmoodusmani693@gmail.com>
2020-06-09 08:20:46 +0500

    Backend Completed

---
+++ quiz/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+from django.forms.widgets import RadioSelect
+
+
+class QuestionForm(forms.Form):
+    def __init__(self, question, *args, **kwargs):
+        super(QuestionForm, self).__init__(*args, **kwargs)
+        choice_list = [x for x in question.get_answers_list()]
+        self.fields["answers"] = forms.ChoiceField(
+            choices=choice_list, widget=RadioSelect)
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username',
+                  'email', 'password1', 'password2']
